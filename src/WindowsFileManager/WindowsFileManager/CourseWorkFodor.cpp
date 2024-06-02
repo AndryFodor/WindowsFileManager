b74a96a6c0f20c8df5a24c2a545d69908394a490
Andry <andrijfodor@gmail.com>
2024-06-02 18:14:36 +0300

    feat: + renameFile fn

--- src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
+++ src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
@@ -11,7 +11,43 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+void renameFile(const fs::path& filePath, const  string& newFileName)
+{
+	if (fs::exists(filePath) && fs::is_regular_file(filePath))
+	{
+		fs::path parentPath = filePath.parent_path();
+		fs::path newFilePath = parentPath / newFileName;
+
+		if (fs::exists(newFilePath))
+		{
+			cout << "A file with the new name already exists." << endl;
+		}
+		else
+		{
+			try
+			{
+				fs::rename(filePath, newFilePath);
+				cout << "File renamed successfully." << endl;
+			}
+			catch (const fs::filesystem_error& e)
+			{
+				cout << "Failed to rename file. Error: " << e.what() << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "Invalid file path." << endl;
+	}
+	_getch();
+}
+
 int main() {
+	string testPath20 = "D://KZP2";
+	string testPath21 = "D://index2.js";
+	string testPath22 = "D://noFile.txt";
+
+	renameFile(testPath21, "index3.js");
 
 
 	return 0;
