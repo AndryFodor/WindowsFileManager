c61044a66c3971ac2e85cbc4bb7070e2a7e8866b
Andry <andrijfodor@gmail.com>
2024-06-02 18:01:55 +0300

    feat: + openFile

--- src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
+++ src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
@@ -11,8 +11,42 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+void openFile(const fs::path& filePath)
+{
+	if (fs::exists(filePath) && fs::is_regular_file(filePath))
+	{
+		ifstream file(filePath);
+		if (file.is_open())
+		{
+			cout << "File Contents: " << endl;
+			cout << "-------------------" << endl;
+
+			string line;
+			while (getline(file, line))
+			{
+				cout << line << endl;
+			}
+
+			file.close();
+		}
+		else
+		{
+			cout << "Failed to open file." << endl;
+		}
+	}
+	else
+	{
+		cout << "Invalid file path." << endl;
+	}
+
+	_getch();
+}
+
 int main() {
+	string testPath11 = "D://index.js";
+	string testPath12 = "D://noFile.txt";
 
-
+	openFile(testPath11);
+	openFile(testPath12);
 	return 0;
 }
