a681b9e906e608d17ffd6a35d465c7b744cefc0b
Andry <andrijfodor@gmail.com>
2024-06-02 17:43:39 +0300

    Implement posibility to create directories

--- src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
+++ src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
@@ -10,9 +10,23 @@
 
 using namespace std;
 namespace fs = std::filesystem;
+void createDirectory(const fs::path& path)
+{
+	if (fs::create_directory(path))
+	{
+		cout << "Directory created successfully." << endl;
+	}
+	else
+	{
+		cout << "Failed to create directory." << endl;
+	}
+
+	_getch();
+}
 
 int main() {
-
-
+	string testPath9 = "D://KZP2";
+	string testPath10 = "D://index3.js";
+	createDirectory(testPath9);
 	return 0;
 }
