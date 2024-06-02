81a5f6099ddb5a840bcf8b9839509ced4f74e889
Andry <andrijfodor@gmail.com>
2024-06-02 14:01:20 +0300

    Project initialization

---
+++ src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <conio.h>
+#include <Windows.h>
+#include <ctime>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+
+
+	return 0;
+}
