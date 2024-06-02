50b87cb492790a6a9fe1a68bc6a5001e824d11e5
Andry <andrijfodor@gmail.com>
2024-06-02 17:15:19 +0300

    Implement displayFileProperties fn

--- src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
+++ src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
@@ -11,8 +11,42 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+void displayFileProperties(const fs::path& path)
+{
+	if (fs::exists(path))
+	{
+		cout << "File Properties: " << endl;
+		cout << "-------------------" << endl;
+		cout << "Path: " << fs::absolute(path) << endl;
+		cout << "Size: " << fs::file_size(path) << " bytes" << endl;
+
+		cout << "Permissions: ";
+		if ((fs::status(path).permissions() & fs::perms::owner_read) != fs::perms::none)
+		{
+			cout << "Read ";
+		}
+		if ((fs::status(path).permissions() & fs::perms::owner_write) != fs::perms::none)
+		{
+			cout << "Write ";
+		}
+		if ((fs::status(path).permissions() & fs::perms::owner_exec) != fs::perms::none)
+		{
+			cout << "Execute ";
+		}
+		cout << endl;
+	}
+	else
+	{
+		cout << "File not found." << endl;
+	}
+
+	_getch();
+}
+
 int main() {
-
-
+	string testPath1 = "D://KZP/kzpLab1.docx";
+	string testPath2 = "D://index.js";
+	string testPath3 = "D://noFile.txt";
+	displayFileProperties(testPath3);
 	return 0;
 }
