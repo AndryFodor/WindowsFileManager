7138ef37824524b729c6293423bd2c7b915dc9f9
Andry <andrijfodor@gmail.com>
2024-06-02 17:36:42 +0300

    add file deliting

--- src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
+++ src/WindowsFileManager/WindowsFileManager/CourseWorkFodor.cpp
@@ -11,8 +11,25 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-int main() {
+void deleteFile(const fs::path& path)
+{
+	if (fs::exists(path))
+	{
+		fs::remove(path);
+		cout << "File deleted successfully." << endl;
+	}
+	else
+	{
+		cout << "File not found." << endl;
+	}
 
+	_getch();
+}
+
+int main() {
+	string testPath7 = "D://KZP2";
+	string testPath8 = "D://index2.js";
+	deleteFile(testPath8);
 
 	return 0;
 }
